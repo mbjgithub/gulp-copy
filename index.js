3b80145f14e8087ab1f54df5d4e320ebac656184
aliarmo <aliarmo@tencent.com>
2018-02-01 18:12:02 +0800

    change copy sync to async

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ module.exports=function(options){
 		errMsg='dest param is needed'
 	}else if(!options.target){
 		errMsg='target param is needed'
+	}else if(!options.target.length){
+		errMsg='target param is empty'
 	}
 	if(errMsg){
     	throw new gutil.PluginError(PLUGIN_NAME, errMsg);
@@ -27,23 +29,41 @@ module.exports=function(options){
             this.emit('error', new PluginError(PLUGIN_NAME, 'Template should not be null!'))
             return cb(null, tpl)
         }
+        var start=+new Date()
         var dest=options.dest
         var target=options.target||[]
         var cnt=tpl.contents
+        var base=PLUGIN_NAME + ': ' + colors.yellow('✔') + ' Copy to '
         var tarPath
         fs.access(dest, function(err) {
 		    if(err){
 		    	fs.mkdirSync(dest)
 		    }
+		    var len=target.length
+		    var curr=0
 		    target.map(function(name){
 		    	tarPath=path.join(dest,name)
-	        	fs.writeFileSync(tarPath,cnt)
-	        	gutil.log(
-		            PLUGIN_NAME + ': ' + colors.yellow('✔') + ' Copy to ' + colors.blue(path.resolve(tarPath)) + ' success'
-		        )
+	        	fs.writeFile(tarPath,cnt,function(err){
+	        		if(err){
+	        			gutil.log(base+ colors.blue(path.resolve(tarPath))+' fail')
+	        		}else{
+	        			gutil.log( base+ colors.blue(path.resolve(tarPath)) + ' success')
+	        		}
+	        		curr++
+	        		if(curr===len){
+	        			_succ()
+	        		}
+	        	})
 	        })
-	        cb(null,tpl)
 		});
-        
+        function _succ(){
+        	cb(null,tpl)
+	        var end=+new Date()
+	        var interval=end-start
+	        var timeStr=interval>1000?(interval/1000).toFixed(2)+'s':interval+'ms'
+	        gutil.log(
+	        	PLUGIN_NAME + ': ' + colors.yellow('✔')	+' finished after '+timeStr
+	       	)
+        }
 	})
 }
